895a88a302a9f4bb81c280bea06d2e10c52e7932
daniel <programmer.daniel.kang@gmail.com>
2023-11-21 12:26:17 +0900

    add imagePullPolicy, add test script

--- k8s/rolling-update/script.sh
+++ k8s/rolling-update/script.sh
@@ -9,3 +9,13 @@ curl <pod-ip>:<container-port>
 
 # response check env
 POD-NAME, POD-IP, NAMESPACE, HOST-NAME, HOST-IP
+
+# rolling-update test
+kubectl apply -f deployment.yaml
+kubectl get pods -o wide
+curl <pod-ip>:8080
+>> 'IMAGE VERSION: 1.0'
+kubectl set image deployment rolling-update k8s-hello=mirlang2/k8s-hello:2.0
+kubectl get pods -o wide
+curl <pod-ip>:8080
+>> 'IMAGE VERSION: 2.0'
